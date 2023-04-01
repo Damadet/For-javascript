fe20addd120ce7042397a4b9a60050f77cee1962
damadet <adedokundamilare41@gmail.com>
2023-04-01 18:52:16 +0200

    9-try

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+    const array = [];
+  
+    try {
+      array.push(mathFunction());
+    } catch (error) {
+      array.push(error.toString());
+    } finally {
+      array.push('Guardrail was processed');
+    }
+    return array;
+}
